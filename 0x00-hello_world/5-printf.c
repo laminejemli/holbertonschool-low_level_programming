28fe6ece0d5788a1aa9232ad7ed842618691fcfa
laminejemli <laminejeemli@gmail.com>
2021-02-03 13:10:32 +0000

    display using PRINTF

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*main = display the phrase with proper grammar , but the outcome is a piece of art 
+*main = display the phrase with proper grammar , but the outcome is a piece of art,
 */
 int main (void)
 {
-printf ("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
