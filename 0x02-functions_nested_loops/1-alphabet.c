1f4c1d675193c349c31ed1809ba35e2b53abf2a1
laminejemli <laminejeemli@gmail.com>
2021-02-09 01:00:58 +0000

    change

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,18 @@
 
 /**
 * print_alphabet - prints the alphabet, in lowercase, followed by a new line
-**/
+*/
 void print_alphabet(void)
 {
-char c;
-c = 'a';
-while (c <= 'z')
+char ch;
+
+ch = 'a';
+
+while (ch <= 'z')
 {
-_putchar('c');
+_putchar(ch);
+ch++;
 }
+
 _putchar('\n');
 }
