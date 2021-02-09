8fd4f60499e3893c067170bcfae94c78636c6d71
laminejemli <laminejeemli@gmail.com>
2021-02-09 00:57:23 +0000

    fixed code

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,10 +4,8 @@
 * main -  prints Holberton, followed by a new line
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-
 _putchar('H');
 _putchar('o');
 _putchar('l');
@@ -18,5 +16,6 @@ _putchar('t');
 _putchar('o');
 _putchar('n');
 _putchar('\n');
+
 return (0);
 }
