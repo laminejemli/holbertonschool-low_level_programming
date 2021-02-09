6d0d5b15c8e29b0f82b6de756b99462fb31d8f5c
laminejemli <laminejeemli@gmail.com>
2021-02-09 00:32:52 +0000

    function C

---
+++ 0x02-functions_nested_loops-/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - print the sign of number 
+ * @n: the int check
+ * Return: 1 and + if n is greater than zero
+ * 0 and prints 0 if n is zero 
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+  if (n > 0)
+    {
+    _putchar('+');
+  return (1);
+    }  else if (n == 0)
+    {
+      _putchar('0');
+    return (0);
+    } else
+    { 
+      _putchar('-1');
+      return (-1);
+    }
+}
