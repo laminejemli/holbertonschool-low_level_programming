012fca05259a54f0281e480a8120b93023265c0f
laminejemli <laminejeemli@gmail.com>
2021-02-11 12:10:16 +0000

    fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
-#include "holberon.h"
-/**
-* _isdigit - checks for a digit (0 through 9)
-* @c: int to be checked
-* Return: 1 if c is a digit, 0 otherwise
-*/
+#include "holberton.h"
 
-int _isdigit(int c)
+/**
+* _isupper - checks for uppercase character
+* @c: the character to be checked
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+int _isupper(int c)
 {
 int c;
 
