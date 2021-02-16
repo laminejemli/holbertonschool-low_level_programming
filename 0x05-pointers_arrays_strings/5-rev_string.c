7f706649798bef5fb2d83b6d40701eaa1637615c
lamine <lamine@lamine.localdomain>
2021-02-16 09:04:51 -0800

    fix code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,13 @@ void rev_string(char *s)
 
 	while (s[len] != '\0')
 	{
-		len++
+		len++;
 	}
 
 	len1 = len - 1;
 	for (i = 0; i < len / 2; i++)
 	{
-		tmp = s[i@];
+		tmp = s[i];
 		s[i] = s[len1];
 		s[len1--] = tmp;
 	}
