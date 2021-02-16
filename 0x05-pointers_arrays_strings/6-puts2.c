ed4fce46122f7a1205dcd1e0e2b327237f8815d2
lamine <lamine@lamine.localdomain>
2021-02-16 03:28:12 -0800

    upgrade

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*puts2 - print every other character
+flowed by a new line
+* @str: the string to be printe
+*/
+void puts2(char *str)
+{
+	int i;
+
+for (i = 0; str[i] != '\0'; i = i+2)
+{
+	_putchar(str[i]);
+}
+		_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *
+ *
+*/
+void puts_half(char *str)
+{
+	int i, l;
+
+	for (i = 0 ; str[i] != '\0'; i++)
+	{
+l = i;
+	}
+	for (l = i/2; str[l] != '\0'; i++)
+	{
+		if str[l]
