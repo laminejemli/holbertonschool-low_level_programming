3751d933ba21eeafc2c413ac0354f0e5585bf162
lamine <mokhtarjemli2@gmail.com>
2021-02-28 13:36:18 -0800

    lol

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,33 @@
-#include "holberton.h"
-#include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
-* main - add  numbers.
-* @argc: number of arguments.
-* @argv: arguments.
-* Return: Always 0.
+* main - print name program
+*@argc:integer
+*@argv:string
+* Return:0
 */
-
 int main(int argc, char *argv[])
 {
-int i, add = 0;
-char *endptr;
+int i, j, Sum;
 
+Sum = 0;
 if (argc == 1)
 printf("0\n");
-else
+if (argc > 1)
 {
-for (i = 1; argv[i] != NULL; i++)
-}															add += strtol(argv[i], &endptr, 10);
-if (*endptr != '\0')
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{															if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 {
 printf("Error\n");
 return (1);
-															}
 }
-
-printf("%d\n", add);
+}
+Sum = Sum + atoi(argv[i]);
+}
+printf("%d\n", Sum);
 }
 return (0);
 }
