62ad6b638bff51ea18c6272b4101a8028b5d545d
lamine <mokhtarjemli2@gmail.com>
2021-03-02 05:39:54 -0800

    lol

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-		return(NULL);
+		return (NULL);
 	s = malloc(sizeof(char) * size);
 	if (s == NULL)
 		return (0);
