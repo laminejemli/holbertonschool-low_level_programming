5ebfd484a510afdb5a07c03e880f86b01a464579
lamine <mokhtarjemli2@gmail.com>
2021-03-03 00:40:21 -0800

    fixed code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- * * **alloc_grid - creates a two dimensional array of ints
- * * @width: width of the matrix
- * * @height: height of the matrix
- * *
- * * Return: pointer to the created matrix (Success)
- * * or NULL (Error)
- * */
+* **alloc_grid - creates a two dimensional array of ints
+* @width: width of the matrix
+* @height: height of the matrix
+* Return: pointer to the created matrix (Success)
+* or NULL (Error)
+*/
 int **alloc_grid(int width, int height)
 {
 	int **arr;
