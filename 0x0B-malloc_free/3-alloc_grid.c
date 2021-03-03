9f259ba8ec79753a60adca5d5d8d4dd0e22b186d
lamine <mokhtarjemli2@gmail.com>
2021-03-03 00:34:09 -0800

    lol

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,10 +26,10 @@ for (i = 0; i < height; i++)
 arr[i] = (int *) malloc(sizeof(int) * width);
 if (arr[i] == NULL)
 {
-free(arr);
-for (j = 0; j <= i; j++)
-free(arr[j]);
-return (NULL);
+	free(arr);
+	for (j = 0; j <= i; j++)
+		free(arr[j]);
+	return (NULL);
 }
 }
 for (i = 0; i < height; i++)
