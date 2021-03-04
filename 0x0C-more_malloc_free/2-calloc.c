192082b854a654517d91f310dc4de38871faae3f
lamine <mokhtarjemli2@gmail.com>
2021-03-04 08:46:19 -0800

    lol

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 #include "holberton.h"
 /**
  * *_memset - fills memory with a constant byte
- * @s: memory area to be filled 
+ * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
  * Return: pointer to the memory area s
@@ -39,4 +39,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
-}	
+}
