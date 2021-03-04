7bfe2517b0d35d06c0b983abfddd0c0fa3899846
lamine <mokhtarjemli2@gmail.com>
2021-03-03 19:09:24 -0800

    lol

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* *malloc_checked - allocates memory using malloc
+* @b: number of bytes to allocate
+*
+* Return: a pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	return (s);
+}
+
