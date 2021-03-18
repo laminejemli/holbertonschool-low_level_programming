f9104f83a9972479404756023b31ec41b2a47a57
lamine <mokhtarjemli2@gmail.com>
2021-03-18 14:18:06 -0700

    fixed code

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,19 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
+
 /**
  * free_list - frees a linked list
  * @head: pointer
  */
 void free_list(list_t *head)
 {
-	if (head == NULL)
+	list_t *tmp;
+
+	while (head)
 	{
+		tmp = head->next;
+		free(head->str);
 		free(head);
-		return;
+		head = tmp;
 	}
-	free_list(head->next);
-	free(head->str);
-	free(head);
 }
