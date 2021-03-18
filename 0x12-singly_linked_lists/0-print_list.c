25377bb16c0a7eba241af1d6b81df6390609c91c
lamine <mokhtarjemli2@gmail.com>
2021-03-18 14:07:51 -0700

    prints a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,20 +2,24 @@
 #include "lists.h"
 
 /**
-* print_list - displays the node of the linked list
-* @h: pointer
-* Return: number of nodes
-*/
+ * print_list - prints the elements
+ * @h: pointer to print
+ *
+ * Return: number of nodes 
+ */
 size_t print_list(const list_t *h)
 {
-size_t z = 0;
-for (z = 0; h != 0; z++)
-{
-if (!h->str)
-printf("[0](nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-}
-return (z);
+	size_t z = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		z++;
+	}
+
+	return (z);
 }
