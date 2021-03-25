1fda2dfa714107ab94a436ab193951232794cd13
lamine <mokhtarjemli2@gmail.com>
2021-03-25 15:04:40 -0700

    fixed betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int base = 0;
-    int i;
+unsigned int base = 0;
+int i;
 
-    if (!b)
-    return (0);
+if (!b)
+return (0);
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		base = 2 * base + (b[i] - '0');
-	}
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+base = 2 * base + (b[i] - '0');
+}
 
-	return (base);
+return (base);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,21 +6,21 @@
  */
 void print_binary(unsigned long int n)
 {
-    unsigned int c;
-    int i, p = 0;
+unsigned int c;
+int i, p = 0;
 
-    for (i = 63; i >= 0; i--)
-    {
-        c = n >> i;
+for (i = 63; i >= 0; i--)
+{
+c = n >> i;
 
-        if (c & 1)
-        {
-                _putchar('1');
-                p++;
-                }
-            else if (c)
-            _putchar('0');
+if (c & 1)
+{
+_putchar('1');
+p++;
 }
-            if (!c)
-            _putchar('0');
-            }
+else if (c)
+_putchar('0');
+}
+if (!c)
+_putchar('0');
+}
