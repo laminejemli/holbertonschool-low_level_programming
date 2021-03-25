16fb6d19a95074d8cb3e512af7aacab91ba39bd0
lamine <mokhtarjemli2@gmail.com>
2021-03-25 00:18:32 -0700

    Bit Manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: pointer to char
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int base = 1;
+    unsigned int i = 0;
+    int a;
+    unsigned int len;
+
+    len = strlen(b);
+
+    for (a = len - 1; a >= 0; a--)
+    {
+        if (b[a] != '0' && b[a] != '1')
+        return (0);
+
+        if (b[a] == '1')
+        {
+            i += base;
+            }
+base *= 2;
+}
+return (i);
+}
+
